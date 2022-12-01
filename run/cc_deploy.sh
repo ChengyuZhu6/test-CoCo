72b2be875fa022f2dfb1712b7f4b4c53236041cb
ChengyuZhu6 <chengyu.zhu@intel.com>
2022-11-30 22:12:42 -0500

    fix kubeadm: 未找到命令 2
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- run/cc_deploy.sh
+++ run/cc_deploy.sh
@@ -132,6 +132,7 @@ install_runtime() {
 }
 
 init_kubeadm() {
+    echo "PATH=$PATH"
     local kubeadm_config_file="/etc/kubeadm/kubeadm.conf"
     # Bootstrap the control-plane node.
     kubeadm init --config "${kubeadm_config_file}"
