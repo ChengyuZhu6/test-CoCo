199d166b51f37e5cd15a2c5bde6c4abd5c4ae242
ChengyuZhu6 <chengyu.zhu@intel.com>
2023-01-31 13:15:01 -0500

    change the format of memory info output
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- serverinfo/serverinfo-stdout.sh
+++ serverinfo/serverinfo-stdout.sh
@@ -12,6 +12,6 @@ echo "\n--------CPU:-------"
 lscpu | grep 'CPU'|egrep -v 'NUMA|Vulnerability'
 echo "\n------Memory:------"
 dmidecode -t memory | grep -A7 Physical|sed 's/^[ \t]*//g'
-dmidecode -t memory | grep -e "Size.*[0-9]" -A8 | awk -f $SCRIPT_FOLDER/mem.awk|sed 's/^[ \t]*//g'
+# dmidecode -t memory | grep -e "Size.*[0-9]" -A8 | awk -f $SCRIPT_FOLDER/mem.awk|sed 's/^[ \t]*//g'
 echo "\n-------Disk:-------"
 lshw -class disk|egrep -v "bus|serial"
