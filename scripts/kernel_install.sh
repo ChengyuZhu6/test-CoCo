0e0588e99a5c65a636aa20bb169ec88c38156b4d
ChengyuZhu6 <chengyu.zhu@intel.com>
2023-02-10 03:26:39 -0500

    Add kernel install script
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

---
+++ scripts/kernel_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+version=$1
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-${version}-1.el8.elrepo.x86_64.rpm
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-headers-${version}-1.el8.elrepo.x86_64.rpm
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-core-${version}-1.el8.elrepo.x86_64.rpm
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-devel-${version}-1.el8.elrepo.x86_64.rpm 
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-modules-${version}-1.el8.elrepo.x86_64.rpm
+wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-modules-extra-${version}-1.el8.elrepo.x86_64.rpm
+yum localinstall kernel-ml-* --allowerasing -y
+rm -f kernel-ml-*
--- setup.sh
+++ setup.sh
@@ -42,20 +42,8 @@ EOF
         if [ $(ls -l /lib/modules | wc -l) -le 1 ]; then
             CENTOS_KERNEL_VERSION=$(uname -r)
             version=${CENTOS_KERNEL_VERSION%%-*}
-            cat <<EOF | tee -a kernel_install.sh
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-${version}-1.el8.elrepo.x86_64.rpm
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-headers-${version}-1.el8.elrepo.x86_64.rpm
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-core-${version}-1.el8.elrepo.x86_64.rpm
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-devel-${version}-1.el8.elrepo.x86_64.rpm 
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-modules-${version}-1.el8.elrepo.x86_64.rpm
-wget --no-check-certificate https://dl.lamp.sh/kernel/el8/kernel-ml-modules-extra-${version}-1.el8.elrepo.x86_64.rpm
-yum localinstall kernel-ml-* --allowerasing -y
-rm -f kernel-ml-*
-EOF
-            chmod +x kernel_install.sh
-            ./kernel_install.sh
+            ./scripts/kernel_install.sh $version
             mv /lib/modules/$version-1.el8.elrepo.x86_64 /lib/modules/$CENTOS_KERNEL_VERSION
-            rm kernel_install.sh
         fi
         if [ ! -f /etc/fstab ]; then
             cat <<EOF | tee -a /etc/fstab
